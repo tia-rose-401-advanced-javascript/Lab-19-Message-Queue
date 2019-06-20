1cb5724fa57b33899bba9933c6eb2e358dfa32a4
Tia Rose <shortyshort789@aol.com>
2019-06-20 14:43:11 -0700

    have database and files queues monitoring appropriate events

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ const Q = require('@nmq/q/server');
 
 Q.start();
 
+const file = new Q('files');
+file.monitorEvent('save');
+file.monitorEvent('error');
+
+
 const db = new Q('database');
 db.monitorEvent('create');
+db.monitorEvent('update');
+db.monitorEvent('read');
+db.monitorEvent('delete');
+db.monitorEvent('error');
